8a70f3cc5f3281b5821517c2bf188108d27596a7
ireina7 <comcx.yang@gmail.com>
2022-06-12 02:25:38 +0800

    update doc

---
+++ java/leetcode/problem41/Solution0.java
@@ -0,0 +1,15 @@
+package leetcode.problem41;
+import java.util.*;
+
+/**
+ * @url https://leetcode.cn/problems/first-missing-positive/
+ * @constraints time O(n), space O(1)
+ * */
+public class Solution0 {
+    public int firstMissingPositive(int[] nums) {
+        return 0;
+    }
+}
+
+
+
