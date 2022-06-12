13765b440eeeb2eb7348ccb7dec227ef316ab55a
ireina7 <comcx.yang@gmail.com>
2022-06-12 12:46:39 +0800

    log

--- java/leetcode/problem41/Solution0.java
+++ java/leetcode/problem41/Solution0.java
@@ -4,10 +4,30 @@ import java.util.*;
 /**
  * @url https://leetcode.cn/problems/first-missing-positive/
  * @constraints time O(n), space O(1)
+ *
+ * The constraints are too strict, although the overall strategy is `dac`
+ * but the implementation has to be imperative.
  * */
 public class Solution0 {
     public int firstMissingPositive(int[] nums) {
-        return 0;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 }
 
---
+++ java/leetcode/problem42/Solution0.java
@@ -0,0 +1,35 @@
+package leetcode.problem42;
+import java.util.*;
+
+/**
+ * https://leetcode.cn/problems/trapping-rain-water/
+ * */
+public class Solution0 {
+    int[] hs;
+    public int trap(int[] height) {
+        if(height.length == 0) {
+            return 0;
+        }
+        this.hs = height;
+        int j = Arrays.stream(hs).max().getAsInt();
+        return dac(0, j);
+    }
+    int dac(int i, int j) {
+        if(i > j) {
+            return 0;
+        }
+        if(i < j) {
+            int k = i + ((j - i) >> 1);
+            int upper = dac(i, k);
+            int lower = dac(k + 1, j);
+            return upper + lower;
+        }
+        if(i == 0) {
+            int sum = 0;
+            int pre = 0;
+            for(int p = 0; p < hs.length; ++p) {
+            
+            }
+        }
+    }
+}
