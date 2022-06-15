8729f0f831c9f9c80b847e22a1472fe8ed7ccaa2
AHGGG <ironpan3160899@gmail.com>
2022-06-15 17:44:24 +0800

    241
    241

---
+++ java/leetcode/problem241/Solution0.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<String, List<Integer>> memo = new HashMap<>();
+    public List<Integer> diffWaysToCompute(String expression) {
+        if(memo.containsKey(expression)) return memo.get(expression);
+        List<Integer> res = new LinkedList<>();
+        if(expression.length() <= 2){
+            res.add(Integer.valueOf(expression));
+            memo.put(expression, res);
+            return res;
+        }
+        
+        for(int i = 0; i < expression.length(); i++){
+            char c = expression.charAt(i);
+            if(c == '+' || c == '-' || c == '*'){
+                List<Integer> leftSub = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> rightSub = diffWaysToCompute(expression.substring(i + 1, expression.length()));
+                for(int left : leftSub){
+                    for(int right : rightSub){
+                        if(c == '+'){
+                            res.add(left + right);
+                        }else if(c == '-'){
+                            res.add(left - right);
+                        }else if(c == '*'){
+                            res.add(left * right);
+                        }
+                    }
+                }
+            }
+        }
+        memo.put(expression, res);
+        return res;
+    }
+}
