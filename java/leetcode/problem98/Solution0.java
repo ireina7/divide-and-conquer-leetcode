b7d1a2ff2ab1ee959ceb1c83397f7ebc3024b921
ireina7 <comcx.yang@gmail.com>
2022-06-20 17:35:57 +0800

    problem 98

---
+++ java/leetcode/problem98/Solution0.java
@@ -0,0 +1,28 @@
+package leetcode.problem98;
+
+public class Solution0 {
+    TreeNode max;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        boolean left = isValidBST(root.left);
+        if (!left) return false;
+        if (max != null && root.val <= max.val) {
+            return false;
+        }
+        max = root;
+        return isValidBST(root.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
