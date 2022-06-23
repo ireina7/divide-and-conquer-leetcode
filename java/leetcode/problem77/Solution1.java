a2860792d64b686d6664597ad5b396ecf477c27c
ireina7 <comcx.yang@gmail.com>
2022-06-23 16:17:28 +0800

    refined problem 77

--- java/leetcode/problem77/Solution1.java
+++ java/leetcode/problem77/Solution1.java
@@ -7,25 +7,25 @@ import java.util.List;
 public class Solution1 {
     List<List<Integer>> ans = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
+    
     public List<List<Integer>> combine(int n, int k) {
-        dfs(n, k, 1);
+        dfs(n, k);
         return ans;
     }
-    
     /**
      * 好吧，这里加上回溯写法。
-     * 每次从集合中选取元素，可选择的范围随着选择的进行而收缩，调整可选择的范围，就是要靠startIndex
-     * @param startIndex 用来记录本层递归的中，集合从哪里开始遍历（集合就是[1,...,n] ）。
+     * 每次从集合中选取元素，可选择的范围随着选择的进行而收缩，调整可选择的范围。
+     * @param j 用来记录本层递归的中，集合从哪里开始遍历（集合就是[1,...,j] ）
      */
-    void dfs(int n, int k, int startIndex) {
-        //终止条件
-        if (path.size() == k) {
+    void dfs(int j, int k) {
+        if (k == 0) {
             ans.add(new ArrayList<>(path));
             return;
         }
-        for (int i = startIndex; i <= n - (k - path.size()) + 1; ++i) {
+        if (j == 0) return;
+        for (int i = k; i <= j; ++i) {
             path.add(i);
-            dfs(n, k, i + 1);
+            dfs(i - 1, k - 1);
             path.removeLast();
         }
     }
