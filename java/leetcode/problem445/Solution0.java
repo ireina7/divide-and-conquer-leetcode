c60f911f6d24779bc348cdecd3f6c873934acf3c
ireina7 <comcx.yang@gmail.com>
2022-06-24 11:28:31 +0800

    problem 445

---
+++ java/leetcode/problem445/Solution0.java
@@ -0,0 +1,29 @@
+package leetcode.problem445;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Solution0 extends Solution {
+    @Override
+    public String describe() {
+        return null;
+    }
+    
+    int[] child;
+    int[] cookie;
+    @Override
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+        this.child = g;
+        this.cookie = s;
+        return dfs(0, 0);
+    }
+    int dfs(int i, int j) {
+        if (i == child.length || j == cookie.length) {
+            return 0;
+        }
+        return cookie[j] >= child[i]
+            ? 1 + dfs(i + 1, j + 1) : dfs(i, j + 1);
+    }
+}
