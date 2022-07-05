d941b4bb5ca554b5bcaaadffec4ce5bf52a8be25
ireina7 <comcx.yang@gmail.com>
2022-07-05 16:17:17 +0800

    problem 968

---
+++ java/leetcode/problem968/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.problem968;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class Solution implements core.Solution {
+    @Override
+    public URL link() throws MalformedURLException {
+        return new URL("https://leetcode.cn/problems/binary-tree-cameras/");
+    }
+    public abstract int minCameraCover(TreeNode root);
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ java/leetcode/problem968/Solution0.java
@@ -0,0 +1,40 @@
+package leetcode.problem968;
+
+public class Solution0 extends Solution {
+    @Override
+    public String describe() {
+        return "Greedy";
+    }
+    
+    int count = 0;
+    @Override
+    public int minCameraCover(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (dfs(root) == 0) count += 1;
+        return count;
+    }
+    
+    /**
+     * return
+     * 0 : 表示没有覆盖到
+     * 1 : 覆盖到了，但是该节点没有摄像头
+     * 2 : 覆盖到了，且该节点有摄像头
+     */
+    public int dfs(TreeNode root) {
+        if (root == null) return 1;
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
+        int left  = dfs(root.left);
+        int right = dfs(root.right);
+        if (left == 0 || right == 0) {
+            count++;
+            return 2;
+        }
+        if (left == 2 || right == 2) {
+            return 1;
+        }
+        return 0;
+    }
+}
